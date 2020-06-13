12836b45071e8e0c9ba5f5326434b943508ae998
zorer <ahmetcj4@hotmail.com>
2020-06-13 04:45:45 +0300

    implement add, show and done task logic via provider instead of dummy updating everything

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -72,10 +72,7 @@ class TasksScreen extends StatelessWidget {
           builder: (context) => SingleChildScrollView(
             child: Container(
               padding: EdgeInsets.only(
-                bottom: MediaQuery
-                  .of(context)
-                  .viewInsets
-                  .bottom,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
               child: AddTaskScreen(),
             ),

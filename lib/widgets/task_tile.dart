756f122b64a4e46dd248a10db643beb06ec5fc53
zorer <ahmetcj4@hotmail.com>
2020-06-13 03:14:16 +0300

    add tile checkbox functionality

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key key,
   }) : super(key: key);
 
   @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool checked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('this is a task'),
+      title: Text(
+        'this is a task',
+        style: TextStyle(
+          decoration: checked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: Checkbox(
-        value: false,
+        value: checked,
+        activeColor: Colors.lightBlueAccent,
+        onChanged: (value) => setState(() => checked = value),
       ),
     );
   }
